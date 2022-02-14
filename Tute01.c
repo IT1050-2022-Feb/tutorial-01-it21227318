9887d8aeb8aeaebca87424b8f09bdb393185cb1c
it21227318 <99632246+it21227318@users.noreply.github.com>
2022-02-14 05:55:59 +0000

    done

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
-  
+  int m1 , m2 ;
+  float tot = 0 ;
+  float ave = 0 ;
+
+  printf("Enter mark 1 - ");
+  scanf("%d",&m1);
+  printf("Enter mark 2 - ");
+  scanf("%d",&m2);
+
+  tot = m1 + m2 ;
+  ave= tot / 2.0 ;
+  printf("average = %.f\n",ave);
+
+
   return 0;
 }
 
